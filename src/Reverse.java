77a25ed83a7c646fe08b4ebf33e23615d07a6420
Fibbinocci <tanakalanaveen2016@gmail.com>
2024-06-26 17:12:26 +0530

    Reverse added successfully

---
+++ src/Reverse.java
@@ -0,0 +1,10 @@
+public class Reverse {
+    public static void main(String[] args){
+        int[] array = {1,2,3,4,5};
+        int size = array.length;
+        for(int i=size-1;i>=0;i--){
+            System.out.print( array [i] );
+        }
+
+    }
+}
